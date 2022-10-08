5829bd6788b0e50038eb9ea2520368d8ea491e5d
DennisTanghe <dennistanghe@gmail.com>
2022-10-08 17:41:51 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+
+import 'primeicons/primeicons.css';
+ 
+import { Sidebar } from 'primereact/sidebar';
+import {Button } from 'primereact/button';
+ 
 import './style.css';
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
+
+<Sidebar visible={visible} onHide={() => setVisible(false)}>
+    Content
+</Sidebar>
+
+<Button icon="pi pi-arrow-right" onClick={(e) => setVisible(true)}/>
+ 
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
